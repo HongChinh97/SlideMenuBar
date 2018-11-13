7febe6b24ef5bda2ca1f2a35e7d2a7406d284e24
admin <chiinhuv@gmail.com>
2018-11-13 17:54:25 +0700

    add slide menu bar

--- SlideMenuBar/ViewController.swift
+++ SlideMenuBar/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isViewMove: Bool = false
+    
+    
+    @IBOutlet weak var leading: NSLayoutConstraint!
+    @IBOutlet weak var bunchView: UIView!
+    
+    
+    @IBAction func animateButton(_ sender: UIBarButtonItem) {
+        if !isViewMove {
+            //tao chuyen dong
+            isViewMove = true
+            UIView.animate(withDuration: 1, animations: {
+                self.leading.constant = self.bunchView.bounds.size.width * 2/3
+                })
+            }
+        else {
+            //tao chuyen dong
+            isViewMove = false
+            self.leading.constant = 0
+            }
+        UIView.animate(withDuration: 1, delay: 0.0, options: .curveEaseIn, animations: {self.view.layoutIfNeeded()})
+    }
+   
+    @IBAction func button(_ sender: Any) {
+        UIView.animate(withDuration: 1) {
+            self.leading.constant = 0
+        }
+        UIView.commitAnimations()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +48,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+
 
 
 }
